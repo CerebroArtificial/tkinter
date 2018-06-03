8d1074e5883411f4eedda1a7ba0234546da3eed3
Vidayaventura <rsgarate@hotmail.com>
2018-06-03 14:20:00 -0400

    Update Window_design_project.py

--- Projects/Window_design_project/Window_design_project.py
+++ Projects/Window_design_project/Window_design_project.py
@@ -7,24 +7,24 @@ class Application():
         self.root = Tk()
         self.root.title("Acces")
         self.root.iconbitmap('favicon.ico')
-        
+               
         source = font.Font(weight='bold')
 
         self.etiq1 = ttk.Label(self.root, text="User:", font=source)
         self.etiq2 = ttk.Label(self.root, text="Password:", font=source)
-        
+
         self.user1 = StringVar()
         self.key = StringVar()
-        
+
         self.user1.set(getpass.getuser())
-        
+
         self.ctext1 = ttk.Entry(self.root, textvariable=self.user1, width=30)
         self.ctext2 = ttk.Entry(self.root, textvariable=self.key, width=30, show="*")
         self.separ1 = ttk.Separator(self.root, orient=HORIZONTAL)
 
         self.button1 = ttk.Button(self.root, text="Accept", command=self.acept1)
         self.button2 = ttk.Button(self.root, text="Cancel", command=quit)
-                                                                        
+                              
         self.etiq1.pack(side=TOP, fill=BOTH, expand=True, padx=5, pady=5)
         self.ctext1.pack(side=TOP, fill=X, expand=True, padx=5, pady=5)
         self.etiq2.pack(side=TOP, fill=BOTH, expand=True, padx=5, pady=5)
@@ -32,7 +32,7 @@ class Application():
         self.separ1.pack(side=TOP, fill=BOTH, expand=True, padx=5, pady=5)
         self.button1.pack(side=LEFT, fill=BOTH, expand=True, padx=5, pady=5)
         self.button2.pack(side=RIGHT, fill=BOTH, expand=True, padx=5, pady=5)
-        
+
         self.ctext2.focus_set()
         self.root.mainloop()
 
